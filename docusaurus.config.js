7dc4927fe9cd210d00dd8bb83f61942f21c548cd
Arthur Lenoir <lenoir.arthur@gmail.com>
2022-05-23 19:16:39 +0200

    feat: add site map optons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,11 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        sitemap: {
+          changefreq: 'daily',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+        },
       }),
     ],
   ],
