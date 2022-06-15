23843c639783dba2453f815a63adcb911399cc3e
Arthur Lenoir <lenoir.arthur@gmail.com>
2022-06-15 21:43:00 +0200

    feat: add people to support committee

--- src/components/SupportCommittee/data.ts
+++ src/components/SupportCommittee/data.ts
@@ -372,7 +372,8 @@ export const supporters: SupporterType[] = [
     firstName: "Mathilde",
     lastName: "Borne",
     city: "Castelnau-le-Lez",
-    occupation: "Conseillère Municipale et Metropolitaine Ville de Castelnau-le-Lez",
+    occupation:
+      "Conseillère Municipale et Metropolitaine Ville de Castelnau-le-Lez",
     message: "",
   },
   {
@@ -380,28 +381,32 @@ export const supporters: SupporterType[] = [
     lastName: "Canet",
     city: "Castries",
     occupation: "Retraitée citoyenne militante",
-    message: "Un monde meilleur est possible avec Julia et la Nupes ! Bravo pour ton engagement et ton enthousiasme !",
+    message:
+      "Un monde meilleur est possible avec Julia et la Nupes ! Bravo pour ton engagement et ton enthousiasme !",
   },
   {
     firstName: "Priscillia",
     lastName: "Mollet",
     city: "Galargues",
     occupation: "Vendeuse",
-    message: "Je soutiens Julia, mon vote ne sera pas présent car ma santé ne me le permet pas. Je tiens donc à écrire mon soutien ici même. Notre pays va mal et avec Macron ça n'est pas prêt d'aller mieux mais de pire en pire comme ça l'est déjà depuis trop longtemps... Je suis de tout cœur avec vous ! Hérault 34 votez Mignacca !!!!! ",
+    message:
+      "Je soutiens Julia, mon vote ne sera pas présent car ma santé ne me le permet pas. Je tiens donc à écrire mon soutien ici même. Notre pays va mal et avec Macron ça n'est pas prêt d'aller mieux mais de pire en pire comme ça l'est déjà depuis trop longtemps... Je suis de tout cœur avec vous ! Hérault 34 votez Mignacca !!!!! ",
   },
   {
     firstName: "Remi",
     lastName: "Serveau",
     city: "Lunel",
     occupation: "",
-    message: "Julia représente l'avenir et je sais qu'elle mènera une politique exemplaire, transparente et citoyenne. Elle a tout mon soutien.",
+    message:
+      "Julia représente l'avenir et je sais qu'elle mènera une politique exemplaire, transparente et citoyenne. Elle a tout mon soutien.",
   },
   {
     firstName: "Denis",
     lastName: "Orcel",
     city: "Montpellier",
     occupation: "Adjoint technique Université de Montpellier",
-    message: "Parce que le changement ne se fera pas sans rupture. Face aux crises sociales, environnementales, économiques il n'est plus question de changer de logiciel mais de système d'exploitation. Je crois dans ce projet collectif mais je resterai vigilent quant à sa mise en œuvre. Votre victoire, notre victoire me laisserait penser que cet autre monde possible est juste là, à portée de main. Tout mon soutien. Force et courage.",
+    message:
+      "Parce que le changement ne se fera pas sans rupture. Face aux crises sociales, environnementales, économiques il n'est plus question de changer de logiciel mais de système d'exploitation. Je crois dans ce projet collectif mais je resterai vigilent quant à sa mise en œuvre. Votre victoire, notre victoire me laisserait penser que cet autre monde possible est juste là, à portée de main. Tout mon soutien. Force et courage.",
   },
   {
     firstName: "Gregory",
@@ -422,13 +427,15 @@ export const supporters: SupporterType[] = [
     lastName: "Carré",
     city: "Castelnau-le-Lez",
     occupation: "Retraitée de l'Éducation Nationale",
-    message: "Le 12/6 et le 19/6 Je vote pour Julia-députée, et le magnifique programme que nous avons élaboré ensemble.",
+    message:
+      "Le 12/6 et le 19/6 Je vote pour Julia-députée, et le magnifique programme que nous avons élaboré ensemble.",
   },
   {
     firstName: "Jacques Antoine",
     lastName: "MARTINI",
     city: "Montpellier",
-    occupation: "Avocat honoraire · Secrétaire Général d'Université en retraite",
+    occupation:
+      "Avocat honoraire · Secrétaire Général d'Université en retraite",
     message: "",
   },
   {
@@ -457,14 +464,16 @@ export const supporters: SupporterType[] = [
     lastName: "Drevon",
     city: "Castelnau-le-Lez",
     occupation: "Agrophysiologiste (ex INRAE) Directeur Scientifique du DEVA",
-    message: "J'apporte mon soutien aux engagement de Julia M pour notre circonscription en particulier dans le champ de mes compétences pour une agriculture écologique et paysanne, et sur les ambitions climatique et les énergies renouvelables.",
+    message:
+      "J'apporte mon soutien aux engagement de Julia M pour notre circonscription en particulier dans le champ de mes compétences pour une agriculture écologique et paysanne, et sur les ambitions climatique et les énergies renouvelables.",
   },
   {
     firstName: "Vincent",
     lastName: "Meynier",
     city: "Clapiers",
     occupation: "Conseiller Municipal",
-    message: "Je partage totalement la vision de l'écologie de Julia avec qui j'ai travaillé sur des problèmes métropolitains dans le passé. J'apprécie son dynamisme et sa rectitude de pensée. Elle fera une excellente députée. J'apporte tout mon soutien actif a sa campagne! Uni.e.s, nous enverrons la gauche et l'écologie à l'Assemblée pour contrer la politique antisociale et écocide du gouvernement Macron 2.",
+    message:
+      "Je partage totalement la vision de l'écologie de Julia avec qui j'ai travaillé sur des problèmes métropolitains dans le passé. J'apprécie son dynamisme et sa rectitude de pensée. Elle fera une excellente députée. J'apporte tout mon soutien actif a sa campagne! Uni.e.s, nous enverrons la gauche et l'écologie à l'Assemblée pour contrer la politique antisociale et écocide du gouvernement Macron 2.",
   },
   {
     firstName: "Fanny",
@@ -485,35 +494,40 @@ export const supporters: SupporterType[] = [
     lastName: "Carrere",
     city: "Vias",
     occupation: "Retraité",
-    message: "Parce que le monde est en danger, parce que la santé va mal, l'éducation va mal, tous nos services publics vont mal, parce que le dérèglement climatique impacte l'avenir de notre territoire et sa capacité à nourrir ses habitants, il faut \"renverser la table\", changer nos moyens de production et de consommation, prendre résolument des directives pour préserver notre environnement et protéger les plus fragiles. Julia incarne ce combat qu'il nous faut mener pour rompre ce cycle infernal où on consomme plus que la terre ne produit et où les plus riches sont toujours plus riches et le nombre de pauvres augmentent toujours plus. Julia doit aller à l'Assemblée National où sa jeunesse, sa pugnacité et sa ténacité permettront de relever ce défi majeur de notre Histoire.",
+    message:
+      "Parce que le monde est en danger, parce que la santé va mal, l'éducation va mal, tous nos services publics vont mal, parce que le dérèglement climatique impacte l'avenir de notre territoire et sa capacité à nourrir ses habitants, il faut \"renverser la table\", changer nos moyens de production et de consommation, prendre résolument des directives pour préserver notre environnement et protéger les plus fragiles. Julia incarne ce combat qu'il nous faut mener pour rompre ce cycle infernal où on consomme plus que la terre ne produit et où les plus riches sont toujours plus riches et le nombre de pauvres augmentent toujours plus. Julia doit aller à l'Assemblée National où sa jeunesse, sa pugnacité et sa ténacité permettront de relever ce défi majeur de notre Histoire.",
   },
   {
     firstName: "Patrick",
     lastName: "Brechotteau",
     city: "Jacou",
     occupation: "Conseiller municipal JACOU citoyenne écologique et sociale",
-    message: "Julia une députée pour porter les revendications écologiques et sociales de la population jacoumarde et de la troisième circonscription de l Hérault à l'Assemblée Nationale.",
+    message:
+      "Julia une députée pour porter les revendications écologiques et sociales de la population jacoumarde et de la troisième circonscription de l Hérault à l'Assemblée Nationale.",
   },
   {
     firstName: "Enzo",
     lastName: "Blondeau",
     city: "Montpellier",
     occupation: "Médiateur scientifique",
-    message: "Portons la NUPES à l'assemblée nationale pour un réel changement social et environnemental ! Merci Julia pour ce combat que vous menez ! Courage, on peut le faire !",
+    message:
+      "Portons la NUPES à l'assemblée nationale pour un réel changement social et environnemental ! Merci Julia pour ce combat que vous menez ! Courage, on peut le faire !",
   },
   {
     firstName: "Dominique",
     lastName: "Nurit",
     city: "Castelnau-Le-Lez",
     occupation: "Psychotherapeute",
-    message: "Julia réunit toutes les valeurs et qualités nécessaires à cette fonction et précieuses pour le virage à prendre concernant le social et l’écologie.",
+    message:
+      "Julia réunit toutes les valeurs et qualités nécessaires à cette fonction et précieuses pour le virage à prendre concernant le social et l’écologie.",
   },
   {
     firstName: "Emmanuel",
     lastName: "Ferrer Minguez",
     city: "Montpellier",
     occupation: "Professeur de SVT · Formateur en botanique",
-    message: "Un souffle d'espoir pour stopper la politique du président. Une alternative pour les classes populaire et moyenne, une alternative pour sauver la fonction publique en particulier l'école et l'hôpital. Un vrai espoir pour enfin mener une politique sociale et écologique. Dimanche, Julia MIGNACCA une dame du peuple, doit siéger à l'assemblée. ",
+    message:
+      "Un souffle d'espoir pour stopper la politique du président. Une alternative pour les classes populaire et moyenne, une alternative pour sauver la fonction publique en particulier l'école et l'hôpital. Un vrai espoir pour enfin mener une politique sociale et écologique. Dimanche, Julia MIGNACCA une dame du peuple, doit siéger à l'assemblée. ",
   },
   {
     firstName: "Arnaud",
@@ -534,14 +548,16 @@ export const supporters: SupporterType[] = [
     lastName: "Masselot",
     city: "Clapiers",
     occupation: "Conseiller municipal délégué",
-    message: "Pour l'écologie politique, pour la justice sociale, pour une démocratie vivante, pour la réduction des inégalités, pour l'avenir, je vote Julia Mignacca",
+    message:
+      "Pour l'écologie politique, pour la justice sociale, pour une démocratie vivante, pour la réduction des inégalités, pour l'avenir, je vote Julia Mignacca",
   },
   {
     firstName: "Gilles",
     lastName: "Dutau",
     city: "Clapiers",
     occupation: "élu Mairie de Clapiers",
-    message: "Pour que Julia puisse porter avec force la parole de l'écologie politique au sein d'une assemblée qui doit rester la boussole de notre République.",
+    message:
+      "Pour que Julia puisse porter avec force la parole de l'écologie politique au sein d'une assemblée qui doit rester la boussole de notre République.",
   },
   {
     firstName: "Annie",
@@ -590,7 +606,8 @@ export const supporters: SupporterType[] = [
     lastName: "Bourguet",
     city: "Montpellier",
     occupation: "Conseillère régionale",
-    message: "Je soutiens sans faille la candidature de Julia Mignacca , Écologiste face à l'urgence écologique, au réchauffement climatique et à la crise sociale. Aucune voix de  ne doit lui manquer dimanche.",
+    message:
+      "Je soutiens sans faille la candidature de Julia Mignacca , Écologiste face à l'urgence écologique, au réchauffement climatique et à la crise sociale. Aucune voix de  ne doit lui manquer dimanche.",
   },
   {
     firstName: "Yassine",
@@ -632,27 +649,99 @@ export const supporters: SupporterType[] = [
     lastName: "Julian",
     city: "Restinclières",
     occupation: "Maire honoraire de Restinclières",
-    message: "Julia Mignacca a tout mon soutien. Elle porte cette union de la gauche si longtemps désirée et aujourd'hui réussie !",
+    message:
+      "Julia Mignacca a tout mon soutien. Elle porte cette union de la gauche si longtemps désirée et aujourd'hui réussie !",
   },
   {
     firstName: "Aurélie",
     lastName: "Santini",
     city: "Sussargues",
     occupation: "Aide ménagère",
-    message: "Je suis fière d'entendre enfin une candidate parler des problèmes du quotidien, de toutes ces injustices que nous subissons ! Il faut des députées comme Julia contre Macron et son monde !",
+    message:
+      "Je suis fière d'entendre enfin une candidate parler des problèmes du quotidien, de toutes ces injustices que nous subissons ! Il faut des députées comme Julia contre Macron et son monde !",
   },
   {
     firstName: "José-Jean",
     lastName: "Taravilla-Camacho",
     city: "Saussines",
     occupation: "Tehatreu",
-    message: "Parce que  nous n'avons plus le choix ... vite vite un  monde  meilleur pour cela je compte sur toi Julia avec tout mon soutien. ",
+    message:
+      "Parce que  nous n'avons plus le choix ... vite vite un  monde  meilleur pour cela je compte sur toi Julia avec tout mon soutien. ",
   },
   {
     firstName: "Thierry",
     lastName: "Jalras",
     city: "Castelnau-le-Lez",
-    occupation: "Professeur agrégé de mathématiques et d'informatique · ingénieur civil des mines",
-    message: "Bien que n'étant pas écologiste mais pour qu'un autre monde soit possible, je soutiens Julia qui apporte un vent de fraîcheur en communiquant efficacement en faveur d'un programme validé par de nombreux économistes de premier plan pour que la fin du monde et la fin du mois ne soient plus un problème pour personne.",
+    occupation:
+      "Professeur agrégé de mathématiques et d'informatique · ingénieur civil des mines",
+    message:
+      "Bien que n'étant pas écologiste mais pour qu'un autre monde soit possible, je soutiens Julia qui apporte un vent de fraîcheur en communiquant efficacement en faveur d'un programme validé par de nombreux économistes de premier plan pour que la fin du monde et la fin du mois ne soient plus un problème pour personne.",
+  },
+  {
+    firstName: "Raphaël",
+    lastName: "Valette",
+    city: "Montpellier",
+    occupation: "Coordinateur local des Jeunes Ecologistes de Montpellier",
+    message: "",
+  },
+  {
+    firstName: "Claude",
+    lastName: "Barral",
+    city: "Lunel",
+    occupation:
+      "Ancien maire ancien vpt du conseil departemental ancien conseiller regional",
+    message: "",
+  },
+  {
+    firstName: "Anne-Sophie",
+    lastName: "Dacher",
+    city: "Restinclières ",
+    occupation: "AESH",
+    message: "Une députée pour porter notre voix !",
+  },
+  {
+    firstName: "Barbara",
+    lastName: "Lambert",
+    city: "Castries",
+    occupation: "Maraîchère en bio",
+    message: "Soutien à fond pour une députée écolo des champs !",
+  },
+  {
+    firstName: "Aurélie",
+    lastName: "Durand",
+    city: "Jacou",
+    occupation: "Aide soignante",
+    message:
+      "En tant qu'aide soignante et déléguée syndicale dans une Maison d’Accueil Spécialisée et Établissement d’accueil médicalisé, je soutiens avec force le programme proposé par la NUPES et la candidate Julia Mignacca qui le porte. Il est urgent pour nous les soignants mais aussi pour les patients et les résidents des structures médico-sociales que des moyens soit alloués pour de meilleurs conditions de travail et d'accueil. Il faut en finir avec ce système de rentabilité permanente qui broient les gens.",
+  },
+  {
+    firstName: "Sébastien",
+    lastName: "Martareche",
+    city: "Jacou",
+    occupation: "Militant associatif",
+    message: "",
+  },
+  {
+    firstName: "Jean-Philippe",
+    lastName: "Chalvin",
+    city: "Saint-Drézéry",
+    occupation: "Testeur logiciel. Militant EELV",
+    message:
+      "J'ai fait la connaissance de Julia il y a quelques mois seulement, mais j'ai suffisamment travaillé avec elle pour apprécier ses grandes qualités. Elle a de fortes convictions écologistes, et des valeurs éthiques. Elle est également pragmatique, efficace, avec les pieds bien campés sur notre Terre. Elle est ouverte d'esprit, capable de faire dialoguer et travailler ensemble des personnes venant de divers horizons. Elle est pleine d'une énergie (renouvelable) capable de soulever des montagnes et d'entraîner derrière elle une équipe de 100 personnes. Elle sera une formidable députée pour notre territoire. Je rembourse votre bulletin de vote si vous n'en êtes pas satisfait à 100% !",
+  },
+  {
+    firstName: "Alexis",
+    lastName: "Boudaud Auduaga",
+    city: "Castelnau-le-Lez",
+    occupation: "Mandataire judiciaire ",
+    message:
+      "L’Écologie Autrement ! soutient Julia pour qu’elle devienne la première députée écologiste sur Montpellier 3",
+  },
+  {
+    firstName: "Pierre",
+    lastName: "Maurel",
+    city: "Clapiers",
+    occupation: "Maire honoraire de Clapiers",
+    message: "",
   },
 ];
